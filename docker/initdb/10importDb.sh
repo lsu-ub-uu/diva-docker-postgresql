8c227680413605574a0a8a59767537176d22c11c
perebartrolisimo <pere.bartroli@ub.uu.se>
2022-12-01 10:39:15 +0000

    initial commit

---
+++ docker/initdb/10importDb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+dbFilesFolder="dbfiles"
+dataDividers="cora diva"
+
+importForDataDivider () {
+	for SQL in "$1"/* ;	do
+		echo "Run SQL file: $SQL"
+		psql -v ON_ERROR_STOP=1 -U systemone systemone < $SQL > $SQL.log
+	done
+}
+
+start(){
+	for dataDivider in $dataDividers ; do
+		echo ""
+		echo "Importing dataDivider: "$dataDivider
+		importForDataDivider $dbFilesFolder"/"$dataDivider
+	done
+}
+
+start
